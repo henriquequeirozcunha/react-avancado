04127964927db886798cd43534c560e55795966f
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-02-17 01:04:42 -0300

    chore: add initial style

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and StyledComponent"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
